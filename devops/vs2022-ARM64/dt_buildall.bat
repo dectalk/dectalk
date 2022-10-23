88b91f3cea795174ccad06063556e56d877fcf62
Leondro Lio <github@leondrolio.com>
2022-10-23 01:33:54 +0100

    visual-studio-2022: Create scripts for vs2022 builds

---
+++ devops/vs2022-ARM64/dt_buildall.bat
@@ -0,0 +1,3 @@
+cd src
+"c:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration="Release - ENGLISH_US" /p:Platform="ARM64"
+cd ..
--- devops/vs2022/dt_buildall.bat
+++ devops/vs2022/dt_buildall.bat
@@ -1,3 +1,4 @@
 cd src
-msbuild.exe
+"c:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration="Release - ENGLISH_US" /p:Platform="AMD64"
+"c:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration="Release - ENGLISH_US" /p:Platform="IA32"
 cd ..
