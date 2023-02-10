42264ff0c2a26c39f67c1e870bd388ce3d615ee3
Leondro Lio <github@leondrolio.com>
2023-02-10 01:43:17 +0000

    cmake: Support Emscripten

--- ports/emscripten/README.md
+++
@@ -1,7 +0,0 @@
-# libdectalk
-
-An emscripten/web port of Fonix DECtalk.
-
-1. Run `yarn build:all` at least once
-2. Run `yarn start` to start a webserver on port 3000
-3. Run `yarn compile` if you change the dapi/`say.c` file.
--- src/emscripten/esbuild.js
+++ src/emscripten/esbuild.js
@@ -2,43 +2,42 @@
 const esbuild = require("esbuild");
 const http = require("http");
 const { copy } = require("esbuild-plugin-copy");
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 
 const argv = yargs(hideBin(process.argv))
   .option("--watch")
-  .option("--production")
-  .argv
+  .option("--production").argv;
 
 const config = {
   entryPoints: ["./src/index.tsx"],
   bundle: true,
-  outfile: "./dist/index.js",
+  outfile: "../../build/bin/index.js",
   plugins: [
     copy({
       // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
       // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
       assets: {
         from: ["./public/*"],
-        to: ["./"]
+        to: ["./"],
       },
     }),
   ],
-  minify: !!argv.production
+  minify: !!argv.production,
 };
 
 if (argv.watch) {
   esbuild
     .serve(
       {
-        servedir: "./dist",
+        servedir: "../../build/bin/",
       },
       config
     )
     .then((result) => {
       // The result tells us where esbuild's local server is
       const { host, port } = result;
-  
+
       // Then start a proxy server on port 3000
       http
         .createServer((req, res) => {
@@ -58,13 +57,13 @@ if (argv.watch) {
               proxyRes.pipe(res, { end: true });
             }
           );
-  
+
           // Forward the body of the request to esbuild
           req.pipe(proxyReq, { end: true });
         })
         .listen(3000);
-  
-      console.log("Listening on port 3000")
+
+      console.log("Listening on port 3000");
     });
 } else {
   esbuild.build(config);
