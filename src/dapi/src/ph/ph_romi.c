9ff0f71f06d6bc1f64feabb0254543010925175b
Aksel Christoffersen <aksel@dectalk.nu>
2026-07-25 16:38:47 +0200

    Add DECtalk Express 4.2CD voice ROM

--- src/dapi/src/ph/ph_romi.c
+++ src/dapi/src/ph/ph_romi.c
@@ -68,6 +68,8 @@
 #include "p_us_rom_dectalk43.c"
 #elif defined(VOICE_ROM_DECTALK_1996M_43F)
 #include "p_us_rom_dectalk_1996m_43f.c"
+#elif defined(VOICE_ROM_DECTALKEXPRESS_42CD)
+#include "p_us_rom_dectalkexpress_42cd.c"
 #elif defined(VOICE_ROM_BETA5)
 #include "p_us_rom.c"
 #else
--- src/dectalkf_klsyn.h
+++ src/dectalkf_klsyn.h
@@ -287,6 +287,7 @@ LANGUAGE	VDEF_FILE	TUNING UP	VDEF_FILE	TUNING UP
 // VOICE_ROM_DECTALK_41 -> voice rom from dectalk 4.1
 // VOICE_ROM_DECTALK_43 -> voice rom from dectalk 4.3
 // VOICE_ROM_DECTALK_1996M_43F -> 1996 rom for male and 4.3 for female voices
+// VOICE_ROM_DECTALKEXPRESS_42CD -> voice rom from dectalk express 4.2cd
 //#define VOICE_ROM_BETA5
 //#define VOICE_ROM_DTC_03_03JAN89
 //#define VOICE_ROM_1997
@@ -294,8 +295,9 @@ LANGUAGE	VDEF_FILE	TUNING UP	VDEF_FILE	TUNING UP
 //#define VOICE_ROM_DECTALK_41
 //#define VOICE_ROM_DECTALK_43
 #define VOICE_ROM_DECTALK_1996M_43F
+//#define VOICE_ROM_DECTALKEXPRESS_42CD
 
-#if defined(VOICE_ROM_1997) || defined(VOICE_ROM_1996) || defined(VOICE_ROM_DECTALK_41) || defined(VOICE_ROM_DECTALK_43) || defined(VOICE_ROM_DECTALK_1996M_43F) || defined(VOICE_ROM_DTC_03_03JAN89)
+#if defined(VOICE_ROM_1997) || defined(VOICE_ROM_1996) || defined(VOICE_ROM_DECTALK_41) || defined(VOICE_ROM_DECTALK_43) || defined(VOICE_ROM_DECTALK_1996M_43F) || defined(VOICE_ROM_DECTALKEXPRESS_42CD) || defined(VOICE_ROM_DTC_03_03JAN89)
 // Decrease /s, z/ amplification in VOICE_ROM_1996/7
 #define DEC_SZ 0
 
